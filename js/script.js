fed8ffefbd331400a717b6a2fbfd089016c6a897
Sakib Md Rezowan <89691048+RezowanSakib@users.noreply.github.com>
2022-02-17 15:32:12 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -1,31 +1,60 @@
+function getExpenses() {
+  const food = Number(document.getElementById("food").value);
+  const rent = Number(document.getElementById("rent").value);
+  const clothes = Number(document.getElementById("clothes").value);
+  if (food >= 0 && (rent >= 0) & (clothes >= 0)) {
+    const expenses = food + rent + clothes;
+    return expenses;
+  } else {
+    return "input value should be positive";
+  }
+}
+function getIncome() {
+  const income = Number(document.getElementById("income").value);
+  return income;
+}
+
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const income = Number(document.getElementById("income").value);
-  const food = Number(document.getElementById("food").value);
-  const rent = Number(document.getElementById("rent").value);
-  const clothes = Number(document.getElementById("clothes").value);
-  const expensess = food + rent + clothes;
+  const income = getIncome();
+  const expenses = getExpenses();
   const balanceText = document.getElementById("balance");
-  const balanceHave = income - expensess;
-
   const totalExpensess = document.getElementById("total-expenses");
-  totalExpensess.innerText = expensess;
-  balanceText.innerText = balanceHave;
-});
-document.getElementById("save-btn").addEventListener('click',function(){
-  
-  const saveInput=Number(document.getElementById("save-input").value)/100;
-  
-  const income = Number(document.getElementById("income").value);
-  
-  const saveBalance=income*saveInput;
-  const remainingBalanceText=document.getElementById("remaing-balance");
-  const food = Number(document.getElementById("food").value);
-  const rent = Number(document.getElementById("rent").value);
-  const clothes = Number(document.getElementById("clothes").value);
-  const expensess = food + rent + clothes;
-  const balanceHave = income - expensess;
-  const remainingBlance=balanceHave-saveBalance;
-  remainingBalanceText.innerText=remainingBlance;
 
-  document.getElementById("saved-money").innerText=saveBalance;
-})
+  if (typeof expenses == "number") {
+    const balanceHave = income - expenses;
+    if (balanceHave >= 0) {
+      balanceText.innerText = balanceHave;
+      totalExpensess.innerText = expenses;
+    } else {
+      totalExpensess.innerText = expenses;
+      balanceText.innerText = "You are spending more than your income";
+    }
+  } else {
+    balanceText.innerText = income;
+    totalExpensess.innerText = expenses;
+  }
+});
+document.getElementById("save-btn").addEventListener("click", function () {
+  const saveInput = Number(document.getElementById("save-input").value) / 100;
+  const income = getIncome();
+
+  const saveBalance = income * saveInput;
+  const remainingBalanceText = document.getElementById("remaing-balance");
+  const expensess = getExpenses();
+  const balanceHave = income - expensess;
+  if (saveBalance >= 0 && balanceHave>=0) {
+    if (balanceHave > saveBalance) {
+      const remainingBlance = balanceHave - saveBalance;
+      remainingBalanceText.innerText = remainingBlance;
+      document.getElementById("saved-money").innerText = saveBalance;
+    } else {
+      document.getElementById("saved-money").innerText =
+        "you do not have money";
+      remainingBalanceText.innerText = balanceHave;
+    }
+  } else {
+    remainingBalanceText.innerText = 0;
+    document.getElementById("saved-money").innerText =
+      "input value should be positive";
+  }
+});
